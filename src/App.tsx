d0d0035cbb33789e0a30cd1316697b335e7302fd
Jstuff36 <mizzou_fan10@hotmail.com>
2018-03-30 15:49:10 -0400

    Create skeleton of navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
 import { HomePage } from './Frontend/Components/HomePage';
+import { NavBar } from './Frontend/Components/NavBar';
 import configureStore from './Frontend/Store/index';
 import { initialState } from './Frontend/Reducers/rootReducer';
 
@@ -10,7 +11,10 @@ class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <HomePage/>
+        <React.Fragment>
+          <NavBar/>
+          <HomePage/>
+        </React.Fragment>
       </Provider>
     );
   }
--- src/Frontend/Components/NavBar.tsx
+++ src/Frontend/Components/NavBar.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
-// function mapStateToProps(state) {
-//     return {};
-// }
-
-export const HomePage = connect()(
-    class HomePage extends React.Component {
+export const NavBar = connect() (
+     class NavBar extends React.Component {
         render() {
             return (
                 <div>
