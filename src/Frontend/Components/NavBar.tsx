525785528cfee2260afd7ec81ac341f192a8c30e
Jstuff36 <mizzou_fan10@hotmail.com>
2018-04-01 13:08:49 -0400

    NavBar skeleton

--- src/Frontend/Components/NavBar.tsx
+++ src/Frontend/Components/NavBar.tsx
@@ -1,12 +1,40 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { List, Icon } from 'semantic-ui-react';
+import './NavBar.styl';
 
 export const NavBar = connect() (
      class NavBar extends React.Component {
         render() {
             return (
-                <div>
-                    hi
+                <div className={'navBarContainer'}>
+                    <List horizontal={true}>
+                        <List.Item>
+                            Home
+                        </List.Item>
+                        <List.Item>
+                            Catalog
+                        </List.Item>
+                        <List.Item>
+                            About
+                        </List.Item>
+                        <List.Item>
+                            Contact Us
+                        </List.Item>
+                    </List>
+                    <List>
+                        <List.Item>
+                            OG Mal Designs
+                        </List.Item>
+                    </List>
+                    <List horizontal={true}>
+                        <List.Item>
+                            <Icon name={'shopping cart'}/>
+                        </List.Item>
+                        <List.Item>
+                            $0.00
+                        </List.Item>
+                    </List>
                 </div>
             );
         }
