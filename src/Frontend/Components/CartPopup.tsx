53b42b4446063a29c1e1f47bca750935fd5a89cf
Jstuff36 <mizzou_fan10@hotmail.com>
2018-05-07 22:56:37 -0400

    Get cart working and apply minor styling

--- src/Frontend/Components/CartPopup.tsx
+++ src/Frontend/Components/CartPopup.tsx
@@ -3,11 +3,12 @@ import { connect } from 'react-redux';
 import { Popup, Icon, List, Button } from 'semantic-ui-react';
 import { StoreState } from '../Reducers/rootReducer';
 import { Item } from '../Reducers/galleryReducer';
+import '../Styles/CartPopup.css';
 
 const mapStateToProps = (store: StoreState) => {
-    const {shoppingCartStore: {selectedItems}} = store;
-    return {selectedItems}
-}
+    const { shoppingCartStore: { selectedItems } } = store;
+    return { selectedItems };
+};
 
 interface StateProps {
     selectedItems: Item[];
@@ -17,7 +18,7 @@ interface State {
     activeItem: Item | null;
 }
 
-type ComponentProps = StateProps
+type ComponentProps = StateProps;
 
 export const CartPopup = connect(mapStateToProps)(
     class CartPopup extends React.Component<ComponentProps, State> {
@@ -25,37 +26,42 @@ export const CartPopup = connect(mapStateToProps)(
         constructor(props: ComponentProps) {
             super(props);
             this.state = {
-                activeItem: null
-            }
+                activeItem: props.selectedItems[0] || null
+            };
+        }
+
+        getImage = (imageUrl: string) => {
+            return { backgroundImage: `url(${imageUrl})` };
         }
 
         render() {
-            const {selectedItems} = this.props;
-            const {activeItem} = this.state;
+            const { selectedItems } = this.props;
+            const { activeItem } = this.state;
             return (
                 <Popup
                     hoverable={true}
                     trigger={<Icon name={'shopping cart'} />}
                     position={'bottom right'}
+                    className={'cartPopupContainer'}
                 >
-                    <div>
-                        <List>
-                            {
-                                selectedItems.map(item => {
-                                    <List.Item>
-                                        <div>
-                                            {item.description}
-                                        </div>
-                                        <div>
-                                            {item.price}
-                                        </div>
-                                    </List.Item>
-                                })
-                            }
-                        </List>
-                    </div>
-                    <div>
-                        {activeItem.url}
+                    <List className={'leftContainer'}>
+                        {
+                            selectedItems.map(item => (
+                                <List.Item key={item.url[0]}>
+                                    <div>
+                                        {item.description}
+                                    </div>
+                                    <div>
+                                        {item.price}
+                                    </div>
+                                </List.Item>
+                            ))
+                        }
+                    </List>
+                    <div className={'rightContainer'}>
+                        <div>
+                            {activeItem ? <div className="image" style={this.getImage(activeItem.url[0])} /> : null}
+                        </div>
                         <Button>Proceed to Checkout</Button>
                     </div>
                 </Popup>
