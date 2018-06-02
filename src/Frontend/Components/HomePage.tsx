705de8d8e21f7bd2d918cfa9a675373e26415827
Jstuff36 <mizzou_fan10@hotmail.com>
2018-06-02 18:56:35 -0400

    Make shop now button work

--- src/Frontend/Components/HomePage.tsx
+++ src/Frontend/Components/HomePage.tsx
@@ -4,6 +4,7 @@ import { Carousel } from './Carousel';
 import { Button } from 'semantic-ui-react';
 import { Footer } from './Footer';
 import { StoreState } from '../Reducers/rootReducer';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 
 function mapStateToProps(store: StoreState) {
     const { homePageStore: { homeCarouselUrls }} = store;
@@ -14,26 +15,35 @@ interface StateProps {
     homeCarouselUrls: string[];
 }
 
-type ComponentProps = StateProps;
+type OwnProps = RouteComponentProps<{}>;
+type ComponentProps = StateProps & OwnProps;
 
-export const HomePage = connect(mapStateToProps)(
-    class HomePage extends React.Component<ComponentProps> {
-        render() {
-            const { homeCarouselUrls } = this.props;
-            return (
-                <div>
-                    <Carousel imgUrls={homeCarouselUrls}/>
+export const HomePage = withRouter(
+    connect<StateProps>(mapStateToProps)(
+        class HomePage extends React.Component<ComponentProps> {
+
+            handleDropdownClick = (item) => {
+                const { history } = this.props;
+                history.push('/gallery');
+            }
+
+            render() {
+                const { homeCarouselUrls, history } = this.props;
+                return (
                     <div>
+                        <Carousel imgUrls={homeCarouselUrls}/>
                         <div>
-                            Check out our collection                                
+                            <div>
+                                Check out our collection                                
+                            </div>
+                            <Button onClick={() => history.push('/gallery')}>
+                                Shop Now
+                            </Button>
                         </div>
-                        <Button>
-                            Shop Now
-                        </Button>
+                        <Footer/>
                     </div>
-                    <Footer/>
-                </div>
-            );
+                );
+            }
         }
-    }
+    )
 );
